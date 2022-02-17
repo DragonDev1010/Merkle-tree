ced1c05ee25ba73e07ef77e63350f5919c03178c
dragonyu1010 <provic0099@gmail.com>
2022-02-18 03:04:52 +0800

    update migration file for nft.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const ZamStacking = artifacts.require("ZamStacking");
 const TestToken = artifacts.require("TestToken")
+const NFT = artifacts.require("ZAMNFT")
 
 module.exports = async function (deployer) {
   	await deployer.deploy(ZamStacking);
   	await deployer.deploy(TestToken, "ZamStake", "ZAM")
+	await deployer.deploy(NFT, "ZamNft", "ZAM", "baseURI");
 };
