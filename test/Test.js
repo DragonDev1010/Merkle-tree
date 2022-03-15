2d438ab961603180ba343b72da94b104b6ae353f
philippine-lime <calamansi.media@gmail.com>
2022-03-15 22:40:45 +0700

    test fixed

--- test/Test.js
+++ test/Test.js
@@ -68,146 +68,147 @@ contract('TrillioHeirs Contract', ([owner, alice_1, bob_2, carol_3, carlos, chad
   })
 
   describe('presale', async () => {
-    // it('should get presale mode correct', async () => {
-    //   expect(await trillioHeirs.presale.call()).to.equal(true)
-    //   await expectRevert(trillioHeirs.setPresale(false, { from: mallory }), 'Ownable: caller is not the owner')
-    //   await trillioHeirs.setPresale(false, { from: owner })
-    //   expect(await trillioHeirs.presale.call()).to.equal(false)
-    //   await expectRevert(trillioHeirs.setPresale(true, { from: owner }), 'TrillioHeirs: presale can be set only once')
-    // })
+    it('should get presale mode correct', async () => {
+      expect(await trillioHeirs.presale.call()).to.equal(true)
+      await expectRevert(trillioHeirs.setPresale(false, { from: mallory }), 'Ownable: caller is not the owner')
+      await trillioHeirs.setPresale(false, { from: owner })
+      expect(await trillioHeirs.presale.call()).to.equal(false)
+      await expectRevert(trillioHeirs.setPresale(true, { from: owner }), 'TrillioHeirs: presale can be set only once')
+    })
 
-    // it('should get paused correct', async () => {
-    //   expect(await trillioHeirs.paused.call()).to.equal(false)
-    //   await expectRevert(trillioHeirs.setPaused(true, { from: mallory }), 'Ownable: caller is not the owner')
-    //   await trillioHeirs.setPaused(true, { from: owner })
-    //   expect(await trillioHeirs.presale.call()).to.equal(true)
-    // })    
+    it('should get paused correct', async () => {
+      expect(await trillioHeirs.paused.call()).to.equal(false)
+      await expectRevert(trillioHeirs.setPaused(true, { from: mallory }), 'Ownable: caller is not the owner')
+      await trillioHeirs.setPaused(true, { from: owner })
+      expect(await trillioHeirs.presale.call()).to.equal(true)
+    })    
 
-    // it('should not mint cause wrong amount', async () => {
-    //   const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
-    //   const wrongProof = tree_1.getHexProof(keccak256(whitelist_1[1]))
-    //   await expectRevert(trillioHeirs.presaleMint(11, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
-    //   await expectRevert(trillioHeirs.presaleMint(1, 4, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
-    //   await expectRevert(trillioHeirs.presaleMint(1, 1, wrongProof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
-    //   await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: bob_2, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
-    //   await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.14', 'ether') }), 'TrillioHeirs: Msg.value is less than the real value')
-    //   await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.24', 'ether') }), 'TrillioHeirs: Msg.value is less than the real value')
-    //   await trillioHeirs.presaleMint(10, 1, proof, { from: alice_1, value: web3.utils.toWei('1.5', 'ether') })
-    //   await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
-    // })
+    it('should not mint cause wrong amount', async () => {
+      const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
+      const wrongProof = tree_1.getHexProof(keccak256(whitelist_1[1]))
+      await expectRevert(trillioHeirs.presaleMint(11, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
+      await expectRevert(trillioHeirs.presaleMint(1, 4, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
+      await expectRevert(trillioHeirs.presaleMint(1, 1, wrongProof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
+      await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: bob_2, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Only whitelisted wallet can attend in presale')
+      await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.14', 'ether') }), 'TrillioHeirs: Msg.value is less than the real value')
+      await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.24', 'ether') }), 'TrillioHeirs: Msg.value is less than the real value')
+      await trillioHeirs.presaleMint(10, 1, proof, { from: alice_1, value: web3.utils.toWei('1.5', 'ether') })
+      await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
+    })
 
-    // it('should not mint cause max presale mint count reached', async () => {
-    //   for (const [index, item] of whitelist_1.entries()) {
-    //     if (index >= 300)
-    //       break
-    //     await trillioHeirs.presaleMint(10, 1, tree_1.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
-    //   }
+    it('should not mint cause max presale mint count reached', async () => {
+      for (const [index, item] of whitelist_1.entries()) {
+        if (index >= 300)
+          break
+        await trillioHeirs.presaleMint(10, 1, tree_1.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
+      }
 
-    //   expect((await trillioHeirs.mintedAmount_1.call()).toString()).to.eql(new BN('3000').toString())
+      expect((await trillioHeirs.mintedAmount_1.call()).toString()).to.eql(new BN('3000').toString())
 
-    //   const proof = tree_2.getHexProof(keccak256(whitelist_2[0]))
-    //   await expectRevert(trillioHeirs.presaleMint(1, 2, proof, { from: bob_2, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: In presale, Only 3000 NFTs can be mint')
-    // }).timeout(5000000)
+      const proof = tree_2.getHexProof(keccak256(whitelist_2[0]))
+      await expectRevert(trillioHeirs.presaleMint(1, 2, proof, { from: bob_2, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: In presale, Only 3000 NFTs can be mint')
+    }).timeout(5000000)
 
-    // it('should not mint cause max level 2 count exceeded', async () => {
-    //   for (const [index, item] of whitelist_2.entries()) {
-    //     if (index >= 150)
-    //       break
-    //     await trillioHeirs.presaleMint(10, 2, tree_2.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
-    //   }
+    it('should not mint cause max level 2 count exceeded', async () => {
+      for (const [index, item] of whitelist_2.entries()) {
+        if (index >= 150)
+          break
+        await trillioHeirs.presaleMint(10, 2, tree_2.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
+      }
 
-    //   expect((await trillioHeirs.mintedAmount_2.call()).toString()).to.eql(new BN('1500').toString())
+      expect((await trillioHeirs.mintedAmount_2.call()).toString()).to.eql(new BN('1500').toString())
 
-    //   const proof = tree_2.getHexProof(keccak256(whitelist_2[160]))
-    //   await expectRevert(trillioHeirs.presaleMint(1, 2, proof, { from: whitelist_2[160], value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Mint amount can not be greater than remaining NFT amount in each level')
-    // }).timeout(5000000)
+      const proof = tree_2.getHexProof(keccak256(whitelist_2[160]))
+      await expectRevert(trillioHeirs.presaleMint(1, 2, proof, { from: whitelist_2[160], value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Mint amount can not be greater than remaining NFT amount in each level')
+    }).timeout(5000000)
 
-    // it('should not mint cause max level 3 count exceeded', async () => {
-    //   for (const [index, item] of whitelist_3.entries()) {
-    //     if (index >= 37)
-    //       break
-    //     await trillioHeirs.presaleMint(10, 3, tree_3.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
-    //   }
+    it('should not mint cause max level 3 count exceeded', async () => {
+      for (const [index, item] of whitelist_3.entries()) {
+        if (index >= 37)
+          break
+        await trillioHeirs.presaleMint(10, 3, tree_3.getHexProof(keccak256(item)), { from: item, value: web3.utils.toWei('1.5', 'ether') })
+      }
 
-    //   expect((await trillioHeirs.mintedAmount_3.call()).toString()).to.eql(new BN('370').toString())
+      expect((await trillioHeirs.mintedAmount_3.call()).toString()).to.eql(new BN('370').toString())
 
-    //   const proof = tree_3.getHexProof(keccak256(whitelist_3[160]))
-    //   await expectRevert(trillioHeirs.presaleMint(1, 3, proof, { from: whitelist_3[160], value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Mint amount can not be greater than remaining NFT amount in each level')
-    // }).timeout(5000000)
+      const proof = tree_3.getHexProof(keccak256(whitelist_3[160]))
+      await expectRevert(trillioHeirs.presaleMint(1, 3, proof, { from: whitelist_3[160], value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: Mint amount can not be greater than remaining NFT amount in each level')
+    }).timeout(5000000)
   })
 
-  // describe('public sale', async () => {
-  //   it('should not mint cause presale enabled', async () => {
-  //     expect(await trillioHeirs.presale.call()).to.equal(true)
-  //     await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') }), 'TrillioHeirs: public sale not started')
-  //   })
+  describe('public sale', async () => {
+    it('should not mint cause presale enabled', async () => {
+      expect(await trillioHeirs.presale.call()).to.equal(true)
+      await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') }), 'TrillioHeirs: public sale not started')
+    })
 
-  //   it('should not mint cause wrong amount', async () => {
-  //     const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
-  //     await trillioHeirs.presaleMint(4, 1, proof, { from: alice_1, value: web3.utils.toWei('0.6', 'ether') })
-  //     await trillioHeirs.setPresale(false, { from: owner })
+    it('should not mint cause wrong amount', async () => {
+      const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
+      await trillioHeirs.presaleMint(4, 1, proof, { from: alice_1, value: web3.utils.toWei('0.6', 'ether') })
+      await trillioHeirs.setPresale(false, { from: owner })
 
-  //     await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: presale finished')
+      await expectRevert(trillioHeirs.presaleMint(1, 1, proof, { from: alice_1, value: web3.utils.toWei('0.15', 'ether') }), 'TrillioHeirs: presale finished')
 
-  //     await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.14', 'ether') }), 'TrillioHeirs: Msg.value is not enough')
-  //     await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.24', 'ether') }), 'TrillioHeirs: Msg.value is not enough')
-  //     await trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') })
-  //     await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
-  //   })
+      await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.14', 'ether') }), 'TrillioHeirs: Msg.value is not enough')
+      await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.24', 'ether') }), 'TrillioHeirs: Msg.value is not enough')
+      await trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') })
+      await trillioHeirs.publicsaleMint(10, { from: alice_1, value: web3.utils.toWei('1.8', 'ether') })
+      await expectRevert(trillioHeirs.publicsaleMint(1, { from: alice_1, value: web3.utils.toWei('0.18', 'ether') }), 'TrillioHeirs: You have already minted max NFTs or you are going to mint too many NFTs now')
+    })
 
-  //   it('should mint random level nft', async () => {
-  //     await trillioHeirs.setPresale(false, { from: owner })
-  //     const res = await trillioHeirs.publicsaleMint(5, { from: alice_1, value: web3.utils.toWei('0.9', 'ether') })
+    it('should mint random level nft', async () => {
+      await trillioHeirs.setPresale(false, { from: owner })
+      const res = await trillioHeirs.publicsaleMint(5, { from: alice_1, value: web3.utils.toWei('0.9', 'ether') })
       
-  //     const lvl1 = [], lvl2 = [], lvl3 = []
-  //     for (const e of res.logs) {
-  //       expect(e.event).to.equal('Transfer')
-  //       const tokenId = e.args.tokenId.toNumber()
-  //       if (tokenId < 7000)
-  //         lvl1.push(tokenId)
-  //       else if (tokenId >= 7000 && tokenId < 8500)
-  //         lvl2.push(tokenId)
-  //       else if (tokenId >= 8500)
-  //         lvl3.push(tokenId)
-  //     }
+      const lvl1 = [], lvl2 = [], lvl3 = []
+      for (const e of res.logs) {
+        expect(e.event).to.equal('Transfer')
+        const tokenId = e.args.tokenId.toNumber()
+        if (tokenId < 7000)
+          lvl1.push(tokenId)
+        else if (tokenId >= 7000 && tokenId < 8500)
+          lvl2.push(tokenId)
+        else if (tokenId >= 8500)
+          lvl3.push(tokenId)
+      }
 
-  //     expect(lvl1.length > 0 && (lvl2.length > 0 || lvl3.length > 0)).to.equal(true)
-  //   })
-  // })
+      expect(lvl1.length > 0 && (lvl2.length > 0 || lvl3.length > 0)).to.equal(true)
+    })
+  })
 
   describe('special list', async () => {
-    // it('should add to special list', async () => {
-    //   await trillioHeirs.setPresale(false, { from: owner })
-    //   await trillioHeirs.addToSpecialList([chad], [3], [1], { from: owner })
-    //   const tx = await trillioHeirs.specialMint(1, { from: chad })
-    //   const tokenId = tx.logs[0].args.tokenId
-    //   expect(await trillioHeirs.ownerOf(tokenId)).to.equal(chad)
-    //   await expectRevert(trillioHeirs.specialMint(1, { from: chad }), 'Trillioheirs: Amount can not be greater than max mint amount')
-    // })
+    it('should add to special list', async () => {
+      await trillioHeirs.setPresale(false, { from: owner })
+      await trillioHeirs.addToSpecialList([chad], [3], [1], { from: owner })
+      const tx = await trillioHeirs.specialMint(1, { from: chad })
+      const tokenId = tx.logs[0].args.tokenId
+      expect(await trillioHeirs.ownerOf(tokenId)).to.equal(chad)
+      await expectRevert(trillioHeirs.specialMint(1, { from: chad }), 'Trillioheirs: Amount can not be greater than max mint amount')
+    })
   })
 
   describe('owner', async () => {
-    // it('should mint 4 lvl', async () => {
-    //   await trillioHeirs.ownerLvl4Mint({ from: owner })
-    //   expect((await trillioHeirs.mintedAmount_4.call()).toString()).to.eql(new BN('18').toString())
-    //   await expectRevert(trillioHeirs.ownerLvl4Mint({ from: owner }), 'TrillioHeirs: level 4 already minted')
-    // })
+    it('should mint 4 lvl', async () => {
+      await trillioHeirs.ownerLvl4Mint({ from: owner })
+      expect((await trillioHeirs.mintedAmount_4.call()).toString()).to.eql(new BN('18').toString())
+      await expectRevert(trillioHeirs.ownerLvl4Mint({ from: owner }), 'TrillioHeirs: level 4 already minted')
+    })
 
-    // it('should be able to withdraw eth', async () => {
-    //   const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
-    //   await trillioHeirs.presaleMint(10, 1, proof, { from: alice_1, value: web3.utils.toWei('1.5', 'ether') })
-    //   const bo1 = await web3.eth.getBalance(owner)
-    //   const b1 = await web3.eth.getBalance(trillioHeirs.address)
-    //   await expectRevert(trillioHeirs.withdrawAll({ from: alice_1 }), 'Ownable: caller is not the owner')
-    //   await trillioHeirs.withdrawAll({ from: owner })
-    //   const bo2 = await web3.eth.getBalance(owner)
-    //   const b2 = await web3.eth.getBalance(trillioHeirs.address)
-    //   expect(b1.toString()).to.eql(ether('1.5').toString())
-    //   expect(b2.toString()).to.eql(ether('0').toString())
-    // })
+    it('should be able to withdraw eth', async () => {
+      const proof = tree_1.getHexProof(keccak256(whitelist_1[0]))
+      await trillioHeirs.presaleMint(10, 1, proof, { from: alice_1, value: web3.utils.toWei('1.5', 'ether') })
+      const bo1 = await web3.eth.getBalance(owner)
+      const b1 = await web3.eth.getBalance(trillioHeirs.address)
+      await expectRevert(trillioHeirs.withdrawAll({ from: alice_1 }), 'Ownable: caller is not the owner')
+      await trillioHeirs.withdrawAll({ from: owner })
+      const bo2 = await web3.eth.getBalance(owner)
+      const b2 = await web3.eth.getBalance(trillioHeirs.address)
+      expect(b1.toString()).to.eql(ether('1.5').toString())
+      expect(b2.toString()).to.eql(ether('0').toString())
+    })
   })
 
-  describe('all counts', async () => {
+  describe.skip('all counts', async () => {
     it('should mint 8888 only', async () => {
       const events = []
       
@@ -253,7 +254,6 @@ contract('TrillioHeirs Contract', ([owner, alice_1, bob_2, carol_3, carlos, chad
         events.push(e.args.tokenId.toNumber())
 
       for (const [index, item] of whitelist_1.entries()) {
-        console.log(index);
         if (index < 180)
           continue
         if (index >= 1317)
